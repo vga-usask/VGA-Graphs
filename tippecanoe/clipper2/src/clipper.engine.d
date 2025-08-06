3e979f5f9280d4df4ecb9f23bbeaef276cad544f
Derek <dwassill@sasktel.net>
2025-08-06 14:03:07 -0600

    another tippecanoe retry

--- tippecanoe/clipper2/src/clipper.engine.d
+++ tippecanoe/clipper2/src/clipper.engine.d
@@ -1,5 +1,5 @@
 clipper2/src/clipper.engine.o: clipper2/src/clipper.engine.cpp \
-  clipper2/include/clipper2/clipper.engine.h \
-  clipper2/include/clipper2/clipper.core.h \
-  clipper2/include/clipper2/clipper.version.h \
-  clipper2/include/clipper2/clipper.h
+ clipper2/include/clipper2/clipper.engine.h \
+ clipper2/include/clipper2/clipper.core.h \
+ clipper2/include/clipper2/clipper.version.h \
+ clipper2/include/clipper2/clipper.h
